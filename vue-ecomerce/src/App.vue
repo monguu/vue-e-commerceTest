fcdbd01da202f539ab278a7870eb074401a3706d
mooonggu <bhnnjm1@naver.com>
2021-03-19 00:25:32 +0900

    test1

---
+++ vue-ecomerce/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: { AppHeader },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
